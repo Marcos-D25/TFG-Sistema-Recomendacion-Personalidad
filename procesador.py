eeee39d4242276536499c820ee89404eae97671a
Mark2518 <marcosdiazsanchez@uma.es>
2026-02-20 15:04:08 +0100

    Segunda version Procesador y Datasets con embeddings

--- procesador.py
+++ procesador.py
@@ -1,17 +1,24 @@
 import os.path
-
+from tqdm import tqdm
 import pandas as pd
 import re
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
+import torch
 
-class Procesador:
-    def __init__(self, nomArchivo, nombre_modelo, max_length=512):
+#Esta clase se encarga de tokenizar y realizar el embedding del dataset a partir de un modelo pre-entrenado
+class Preprocesador:
+    def __init__(self, nomArchivo, nombre_modelo, max_length=512, dispotivo='cuda', columna="posts"):
         print(f"[INFO] Cargando dataset desde: {nomArchivo}")
         self.dataset = pd.read_csv(nomArchivo)
-
+        self.nombre_modelo = nombre_modelo
+        self.columna = columna
         print(f"[INFO] Cargando tokenizador: {nombre_modelo}")
         self.tokenizer = AutoTokenizer.from_pretrained(nombre_modelo)
         self.max_length = max_length
+
+        print(f"[INFO] Cargando modelo: {nombre_modelo}")
+        self.modelo = AutoModel.from_pretrained(nombre_modelo).to(dispotivo)
+        self.modelo.eval()  # Establece el modelo en modo evaluación para evitar dropout y otros comportamientos de entrenamiento
 
         self.patron_ruido = r"\|{3}|https?://\S+|www\.\S+|~|:\S+:"
         self.patron_mbti = r"\b(INTJ|INTP|ENTJ|ENTP|INFJ|INFP|ENFJ|ENFP|ISTJ|ISFJ|ESTJ|ESFJ|ISTP|ISFP|ESTP|ESFP)\b" # \b asegura que solo borre la palabra exacta
@@ -27,10 +34,6 @@ class Procesador:
 
         return post_limpio
 
-    def preprocesar_dataset(self, columna):
-        self.dataset["posts"] = self.dataset[columna].apply(self.limpiar_texto)
-        print("[INFO] ¡Limpieza completada!")
-        return self.dataset
 
     def tokenizar_texto(self, texto):
         tokens = self.tokenizer(
@@ -45,36 +48,92 @@ class Procesador:
         )
         return tokens
 
-    def procesar_dataset(self, columna):
-        self.dataset = self.dataset[columna].apply(self.tokenizar_texto)
 
-    def guardar_dataset(self, direccion):
-        self.dataset.to_csv(direccion, index=False)
+    def aplicarMeanPooling(self, token_embeddings, attention_mask):
+        #Cada post se ha dividido en fragmentos de 512 tokens, por lo que tenemos un tensor de [num_fragmentos, 512, 768]
+        #512 Hace referencia al numero maximo de tokens que el modelo puede procesar, y 768 es la dimensión del embedding de cada token
+        #La dimension del attention_mask es [num_fragmentos, 512]. Tiene un 1 para cada token real y un 0 para cada token de relleno (padding)
+        #Para poder hacer la media de los vectores de cada fragmento, necesitamos expandir la mascara de atención para que tenga la misma dimensión que los embeddings de los tokens
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float() #Aqui ampliamos la mascara de atención para que tenga la misma dimensión que los embeddings de los tokens
+        sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1) # Multiplicamos los embeddings de los tokens por la mascara de atención expandida para que los tokens de relleno no contribuyan a la suma
+        #La suma que realizamos es en la dimension de los tokens, por lo que obtenemos un tensor de [num_fragmentos, 768] que es la suma de los embeddings de los tokens reales de cada fragmento
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9) #Averiguamos el total de tokens reales (no padding)
+        return sum_embeddings / sum_mask #Aplicamos la media dividiendo la suma de los embeddings por el total de tokens reales para obtener un vector de embedding representativo de cada fragmento
 
-def ejecutar_procesador(procesador:Procesador, nombre_modelo:str):
-    print("[EJECUCION] Ejecutando procesador ...]")
-    procesador.preprocesar_dataset("posts")#Funcion de limpieza
-    print("[EJECUCION] Funcion limpieza hecha ...]")
-    procesador.procesar_dataset("posts")#Funcion de tokenizacion
-    print("[EJECUCION] Funcion tokenizacion hecha ...]")
-    procesador.guardar_dataset(os.path.join("datasets",nombre_modelo))
-    print("[EJECUCION] Dataset guardado ...]")
+    def extraer_embeddings(self, tokens):
+        #Recuperamos los tensores de input_ids y attention_mask del dataset y los subimos a la VRAM
+        input_ids = tokens['input_ids'].to(self.modelo.device)
+        attention_mask = tokens['attention_mask'].to(self.modelo.device)
+
+        with torch.no_grad(): #Solo estamos usando el modelo para inferencia, no necesitamos calcular gradientes
+            salida = self.modelo(input_ids=input_ids, attention_mask=attention_mask)
+        
+        token_embedding = salida.last_hidden_state #Recuperamos la salida de la última capa oculta (embedding de cada token)
+        
+        chunk_embeddings = self.aplicarMeanPooling(token_embedding, attention_mask) #Aplicamos mean pooling a nivel de fragmento para obtener un vector por cada fragmento
+
+        embedding_final = torch.mean(chunk_embeddings, dim=0) #Hacemos la media de los vectores de cada fragmento para obtener un solo vector que represente todo el post
+
+        return embedding_final.cpu().numpy().flatten() #Bajamos el vector de la VRAM a la CPU y lo convertimos de una matriz (1, 768) a un vector (768,)
+
+    def procesar_dataset(self):
+        #Esta funcion se encarga de, a partir del texto del dataset original, generar y guardar el embedding en una nueva columna llamada "embedding"
+
+        #Paso 1: Limpiar el texto
+        self.dataset[self.columna] = self.dataset[self.columna].apply(self.limpiar_texto)
+        print("[INFO] ¡Limpieza completada!")
+
+        # Paso 2: Tokenización y Embedding (Pesado - GPU)
+        print(f"[INFO] Procesando embeddings con {self.nombre_modelo}...")
+        
+        embeddings_finales = []
+        
+        for texto in tqdm(self.dataset[self.columna], desc="Extrayendo Embeddings"): #Proceesamos cada post uno por uno. Tambien visualizamos el progreso con tqdm
+            tokens = self.tokenizar_texto(texto)
+            vector = self.extraer_embeddings(tokens)
+            embeddings_finales.append(vector)
+
+        # Paso 3: Preparar DataFrame final
+        print("[INFO] Consolidando resultados...")
+        df_final = pd.DataFrame({
+            "MBTI": self.dataset["type"],
+            "Embedding": [emb.tolist() for emb in embeddings_finales]
+        })
+
+        # Paso 4: Guardado en Parquet
+        os.makedirs("datasets", exist_ok=True)
+        modelo_limpio = self.nombre_modelo.replace("/", "_")
+        ruta_archivo = os.path.join("datasets", f"{modelo_limpio}_dataset.parquet")
+        
+        df_final.to_parquet(ruta_archivo, engine="pyarrow")
+        print(f"[EXITO] Dataset guardado en: {ruta_archivo}")
+
+
+
+def ejecutar_preprocesador(preprocesador:Preprocesador):
+    print("[EJECUCION] Ejecutando preprocesador ...]")
+
+    preprocesador.procesar_dataset()
+
+    print("[EJECUCION] Fin preprocesador ...]")
+
 
 def main():
+    '''
     print("[EJECUCION] Ejecutando ROBERTA BASE...]")
-    robertaBase = Procesador(os.path.join("datasets","MBTI_sinProcesar.csv"),"FacebookAI/roberta-base")
-    ejecutar_procesador(robertaBase, "roberta-base")
+    robertaBase = Preprocesador(os.path.join("datasets","MBTI_sinProcesar.csv"),"FacebookAI/roberta-base")
+    ejecutar_preprocesador(robertaBase)
     print("[EJECUCION] ROBERTA BASE guardado]\n\n")
-
+    '''
     print("[EJECUCION] Ejecutando XML ROBERTA BASE...]")
-    xml_robertaBase = Procesador(os.path.join("datasets","MBTI_sinProcesar.csv"),"FacebookAI/xlm-roberta-base")
-    ejecutar_procesador(xml_robertaBase, "xml-roberta-base")
+    xml_robertaBase = Preprocesador(os.path.join("datasets","MBTI_sinProcesar.csv"),"FacebookAI/xlm-roberta-base")
+    ejecutar_preprocesador(xml_robertaBase)
     print("[EJECUCION] XML ROBERTA BASE guardado]\n\n")
 
     print("[EJECUCION] Ejecutando XML ROBERTA LARGE...]")
-    xml_robertaLarge = Procesador(os.path.join("datasets","MBTI_sinProcesar.csv"),"FacebookAI/xlm-roberta-large")
-    ejecutar_procesador(xml_robertaLarge, "xml-roberta-large")
+    xml_robertaLarge = Preprocesador(os.path.join("datasets","MBTI_sinProcesar.csv"),"FacebookAI/xlm-roberta-large")
+    ejecutar_preprocesador(xml_robertaLarge)
     print("[EJECUCION] XML ROBERTA LARGE guardado]\n\n")
-
+    
 if __name__ == "__main__":
     main()
